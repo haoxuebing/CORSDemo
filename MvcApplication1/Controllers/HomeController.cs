65e221b406034b5674d1c4df57af79c5a0d4be53
haoxuebing <haoxuebing@iyunxiao.com>
2017-05-26 17:06:20 +0800

    first commit

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MvcApplication1.Filter;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Cors(".*")]
+        public JsonResult CorsApi()
+        {
+            return Json(new { data = "跨域请求成功" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [Cors]
+        public ActionResult About()
+        {
+            HttpWebRequest http = (HttpWebRequest)HttpWebRequest.Create("http://localhost:20735/home/CorsApi");
+            var reader = new StreamReader(http.GetResponse().GetResponseStream(), Encoding.UTF8).ReadToEnd();
+            ViewBag.result = reader;
+            return View();
+        }
+
+    }
+}
---
+++ MvcApplication1/Filter/CorsAttribute.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Filter
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.All, Inherited = true, AllowMultiple = true)]
+    public class CorsAttribute : ActionFilterAttribute, IActionFilter
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="AllowOriginsPattern"></param>
+        public CorsAttribute(string allowOriginsPattern = "")
+        {
+            if (string.IsNullOrEmpty(allowOriginsPattern))
+            {
+                this.AllowOriginsPattern = ConfigurationManager.AppSettings["AllowOriginsPattern"].ToString();
+            }
+            else
+            {
+                this.AllowOriginsPattern = allowOriginsPattern;
+            }
+        }
+
+        /// <summary>
+        /// 访问完成，追加head
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            try
+            {
+                base.OnResultExecuted(filterContext);
+                GetResponse();
+            }
+            catch (Exception exception)
+            {
+                HttpContext.Current.Response.Clear();
+                HttpContext.Current.Response.Write(exception.Message);
+                HttpContext.Current.Response.End();
+            }
+        }
+
+
+        /// <summary>
+        /// 允许的正则表达式
+        /// </summary>
+        public string AllowOriginsPattern { get; set; }
+
+        /// <summary>
+        /// 获取response
+        /// </summary>
+        /// <returns></returns>
+        public HttpResponse GetResponse()
+        {
+            HttpRequest request = HttpContext.Current.Request;
+            IDictionary<string, string> headers;
+            bool IsEvaluate = TryEvaluate(HttpContext.Current.Request, out headers);
+            if (IsEvaluate)
+            {
+                foreach (var item in headers)
+                {
+                    HttpContext.Current.Response.Headers.Add(item.Key, item.Value);
+                }
+            }
+            return HttpContext.Current.Response;
+        }
+
+        /// <summary>
+        /// 是否匹配
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        public bool TryEvaluate(HttpRequest request, out IDictionary<string, string> headers)
+        {
+            headers = null;
+            if (request.Headers.GetValues("Origin") != null)
+            {
+                string origin = request.Headers.GetValues("Origin").First();
+                Regex regex = new Regex(AllowOriginsPattern, RegexOptions.IgnoreCase);
+                if (regex.IsMatch(origin))//匹配正则
+                {
+                    headers = this.GenerateResponseHeaders(request);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 生成head
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private IDictionary<string, string> GenerateResponseHeaders(HttpRequest request)
+        {
+            string origin = request.Headers.GetValues("Origin").First();
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("Access-Control-Allow-Origin", origin);
+            headers.Add("Access-Control-Allow-Headers", "x-requested-with,content-type，requesttype,Token");
+            headers.Add("Access-Control-Allow-Methods", "POST,GET");
+            return headers;
+        }
+
+    }
+}
